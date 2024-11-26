914a8032df6621b91174bd93357b42f8983f8101
igiordano <109120850+igiordano@users.noreply.github.com>
2024-11-26 01:34:10 -0300

    Delete PROYECTOS directory

--- PROYECTOS/MVT2024/app2/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(Usuarios)
-admin.site.register(Productos)
-admin.site.register(Ventas_detalles)
-#admin.site.register(MensajeContacto)
